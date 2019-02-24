3880a9713982e7ce7a3390f288d979dfc9792a51
Deka <Deka@DESKTOP-3QJUVTC>
2019-02-24 23:42:36 +0700

    New Version v4.17 Edit getAreas + CarouselJam

--- src/main/java/com/imi/dolphin/sdkwebservice/service/ServiceImp.java
+++ src/main/java/com/imi/dolphin/sdkwebservice/service/ServiceImp.java
@@ -1034,6 +1034,9 @@ public class ServiceImp implements IService {
         //Button 1
         ButtonTemplate button1 = new ButtonTemplate();
         button1.setTitle("08:00 - 10:00");
+        button1.setSubTitle("");
+        button1.setPictureLink(null);
+        button1.setPicturePath(null);
         List<EasyMap> actions1 = new ArrayList<>();
         EasyMap butAction11 = new EasyMap();
         EasyMap butAction12 = new EasyMap();
@@ -1055,6 +1058,9 @@ public class ServiceImp implements IService {
         //Button 2
         ButtonTemplate button2 = new ButtonTemplate();
         button2.setTitle("11:00 - 13:00");
+        button2.setSubTitle("");
+        button2.setPictureLink(null);
+        button2.setPicturePath(null);
         List<EasyMap> actions2 = new ArrayList<>();
         EasyMap butAction21 = new EasyMap();
         EasyMap butAction22 = new EasyMap();
@@ -1077,6 +1083,9 @@ public class ServiceImp implements IService {
         //Button 3
         ButtonTemplate button3 = new ButtonTemplate();
         button3.setTitle("14:00 - 16:00");
+        button3.setSubTitle("");
+        button3.setPictureLink(null);
+        button3.setPicturePath(null);
         List<EasyMap> actions3 = new ArrayList<>();
         EasyMap butAction31 = new EasyMap();
         EasyMap butAction32 = new EasyMap();
@@ -1099,6 +1108,9 @@ public class ServiceImp implements IService {
         //Button 4
         ButtonTemplate button4 = new ButtonTemplate();
         button4.setTitle("17:00 - 19:00");
+        button4.setSubTitle("");
+        button4.setPictureLink(null);
+        button4.setPicturePath(null);
         List<EasyMap> actions4 = new ArrayList<>();
         EasyMap butAction41 = new EasyMap();
         EasyMap butAction42 = new EasyMap();
@@ -1121,6 +1133,9 @@ public class ServiceImp implements IService {
         //Button 5
         ButtonTemplate button5 = new ButtonTemplate();
         button5.setTitle("20:00 - 21:00");
+        button5.setSubTitle("");
+        button5.setPictureLink(null);
+        button5.setPicturePath(null);
         List<EasyMap> actions5 = new ArrayList<>();
         EasyMap butAction51 = new EasyMap();
         EasyMap butAction52 = new EasyMap();
